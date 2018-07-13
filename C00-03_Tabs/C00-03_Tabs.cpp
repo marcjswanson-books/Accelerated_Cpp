89a40facae0d45eba9bebf6fa21bcb40480ce618
marc <marcjswanson@hotmail.com>
2018-07-13 06:39:26 -0500

    First Commit

---
+++ C00-03_Tabs/C00-03_Tabs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+	std::cout << "0123456789012345678901234567890123456789012345678901234567890123456789" << std::endl;
+	std::cout << "\t|\t|\t|\t|\t|\t|\t|\t|" << std::endl;
+
+	return 0;
+}
