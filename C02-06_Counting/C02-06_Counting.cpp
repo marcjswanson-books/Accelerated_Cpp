b5de32c46b79e0cbacca97dc5ca49d1201642443
marc <marcjswanson@hotmail.com>
2018-07-19 06:57:05 -0500

    First Commit

---
+++ C02-06_Counting/C02-06_Counting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		i += 1;
+		std::cout << i << std::endl;
+	}
+
+
+	return 0;
+}
---
+++ C02-07_Countdown/C02-07_Countdown.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv)
+{
+	for ( int x = 10; x > - 6; --x )
+		cout << x << endl;
+
+	return 0;
+}
